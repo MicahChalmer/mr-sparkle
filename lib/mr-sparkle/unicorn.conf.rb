6eeddf0eb57145cee491574fd7b47dbc05c6b1ee
Jonathan Davies <jonnie@obdev.co.uk>
2013-08-20 13:28:25 +0100

    bundler now also loads rack_env gems

--- lib/mr-sparkle/unicorn.conf.rb
+++ lib/mr-sparkle/unicorn.conf.rb
@@ -4,5 +4,5 @@ GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
 
 before_fork do |server, worker|
   require 'bundler'
-  Bundler.require(:default)
+  Bundler.require(:default, ENV["RACK_ENV"])
 end
